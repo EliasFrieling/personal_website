4c52f47445b3222eaaba835788f7c4e3ee9487bc
Elias Frieling <eliasfrieling@gmail.com>
2021-08-10 18:48:19 -0400

    removed unused import

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { MdKeyboardArrowRight, MdArrowForward } from "react-icons/md";
-import { Link } from "react-router-dom";
 export const HeroContainer = styled.div`
   background: #0c0c0c;
   display: flex;
