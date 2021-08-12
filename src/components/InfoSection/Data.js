3a5d8bedf40d9c3de1068b3a8828af66dd365087
Elias Frieling <eliasfrieling@gmail.com>
2021-08-12 11:52:03 -0400

    text updates

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -6,8 +6,8 @@ export const homeObjOne = {
   topLine: "About Me",
   headline: "Hi! I'm Elias",
   description:
-    "I am a 4th year student at Stevens Institute of Technology where I am studying Computer Science and part of the 5 year Co-op program. " +
-    "Some of my hobbies inclue skiing, bjj, and watching the New Jersey Devils. Feel free to contact me through my Twitter or email if you" +
+    "I am a 4th year student at Stevens Institute of Technology where I am studying Computer Science and as a part of the 5 year Co-op program. " +
+    "Some of my hobbies inclue skiing, jiu jitsu, and watching the New Jersey Devils. Feel free to contact me through my Twitter or email if you" +
     " have any questions!",
   imgStart: false,
   alt: "Me",
