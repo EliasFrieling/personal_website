7beb884ceefc647e73386c00648cdeaf75f4a493
Elias Frieling <eliasfrieling@gmail.com>
2022-08-08 10:32:26 -0400

    Added Meta internship data

--- src/components/ResumeSection/index.js
+++ src/components/ResumeSection/index.js
@@ -1,60 +1,63 @@
-import React from "react";
+import React from 'react';
 import {
+  Company,
+  Description,
+  Position,
   ResumeContainer,
   ResumeWrapper,
-  Title,
   TextWrapper,
-  Company,
-  Position,
-  Description,
-} from "./ResumeElements";
+  Title,
+} from './ResumeElements';
 const ResumeSection = () => {
   return (
     <>
-      <ResumeContainer id='experience'>
+      <ResumeContainer id="experience">
         <ResumeWrapper>
           <TextWrapper>
             <Title>Work Experience</Title>
-            <Company>MEMX</Company>
-            <Position>Full Stack Intern • June 2021 - Present</Position>
+            <Company>Meta</Company>
+            <Position>Software Engineer Intern • May 2022 - Aug 2022</Position>
             <Description>
-              • Created a tool that allows a user to inject an order into the
-              stock exchange through a React frontend allowing for easier
-              testing of the exchange
-              <br />• Wrote data validation checks on a NodeJS server that would
-              validate all the fields a user enters in the order and transform
-              the data to match the exchange server API specifications
-              <br />• Populated a table with live market data that updates every
-              ten seconds with price changes using a NodeJS websocket allowing
-              users to view up to date prices when entering in orders
+              • Developed a custom extension for executing GraphQL queries in Bento, an internal
+              version of Jupyter Notebooks using React and Python, automating authentication and
+              execution, saving on average five minutes per notebook
+              <br />• Integrated with the Bento platform by connecting to the Language Server
+              Protocol and adding support for using variables in queries throughout a notebook
+              <br />• Created and executed a plan to refactor areas of the codebase, leading to
+              additional functionality for extensions with support for Web Assembly LSP servers, and
+              an increased number of inherited features from the Bento platform
+            </Description>
+            <Company>MEMX</Company>
+            <Position>Software Engineer Intern • Jun 2021 - May 2022</Position>
+            <Description>
+              • Implemented an order entry tool to enter, view, replace, and cancel orders in the
+              stock exchange, using React and Node.js over a RESTful API, leading to greater
+              efficiency in testing the exchange and onboarding clients
+              <br />• Displayed live market data to the user by using a WebSocket to receive real
+              time market data changes, giving up to date pricing information to users as they use
+              the order entry tool
+              <br />• Collaborated with a designer, product manager, and future users to build an
+              intuitive user experience (UX) utilizing conditional component rendering and
+              client-side error checking
             </Description>
             <Company>McKinsey & Company</Company>
-            <Position>
-              Software Development Intern • September 2020 – December 2020
-            </Position>
+            <Position> Software Engineer Intern • Sep 2020 - Dec 2020</Position>
             <Description>
-              • Migrated steps in the recruiting pipeline to cloud based
-              technology by developing Python scripts to redact data in resumes
-              and perform data validation leading to a streamlined recruiting
-              process
-              <br />• Developed and documented the creation of reusable assets
-              in AWS showing the advantages of moving company services to a
-              cloud based environment
-              <br />• Optimized scripts by evaluating the efficiency and
-              developing fixes to greatly increase speed
+              • Migrated steps in the recruiting pipeline to cloud-based technology by constructing
+              Python scripts running in Amazon Web Services to redact data from resumes in S3
+              buckets, automating steps of the recruiting process
+              <br />• Developed and documented code in AWS as part of a proof-of-concept project
+              highlighting the advantages of moving company services to a cloud-based computing
+              environment
             </Description>
             <Company>MEMX</Company>
-            <Position>
-              Software Development Intern • January 2020 – May 2020
-            </Position>
+            <Position>Software Development Intern • Jan 2020 – May 2020</Position>
             <Description>
-              • Imported data from an API to an internal data tool allowing for
-              broad visualization of stock market data and documented the steps
-              of how to import various file types allowing easier use of the
-              data tool
-              <br />• Designed and implemented software that sends and receives
-              specialized messages to financial institutions as part of the post
-              trade system of the stock exchange
+              • Imported data from an API to an internal data tool allowing for broad visualization
+              of stock market data and documented the steps of how to import various file types
+              allowing easier use of the data tool
+              <br />• Designed and implemented software that sends and receives specialized messages
+              to financial institutions as part of the post trade system of the stock exchange
             </Description>
           </TextWrapper>
         </ResumeWrapper>
